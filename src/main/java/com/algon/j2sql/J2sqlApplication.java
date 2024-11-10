3aaba7fb1d6f3009216f6a9cb72156c2384d9280
kinderproc <kinderproc84@yandex.ru>
2024-11-10 13:01:04 +0300

    feat: Runner added

--- src/main/java/com/algon/j2sql/J2sqlApplication.java
+++ src/main/java/com/algon/j2sql/J2sqlApplication.java
@@ -3,7 +3,7 @@ package com.algon.j2sql;
 public class J2sqlApplication {
 
 	public static void main(String[] args) {
-
+		new Runner().run(args);
 	}
 
 }
---
+++ src/main/java/com/algon/j2sql/Runner.java
@@ -0,0 +1,11 @@
+package com.algon.j2sql;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Runner {
+
+    public void run(String[] args) {
+    }
+
+}
